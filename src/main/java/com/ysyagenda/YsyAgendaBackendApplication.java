91a21b34a2785ad12387acb296b72df9b8e62149
Mariano Van Staden <marianovanstaden@gmail.com>
2024-10-15 20:20:14 -0300

    Last Fixes

--- src/main/java/com/ysyagenda/YsyAgendaBackendApplication.java
+++ src/main/java/com/ysyagenda/YsyAgendaBackendApplication.java
@@ -9,14 +9,16 @@ import com.ysyagenda.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
 
+
 @SpringBootApplication
-public class YsyAgendaBackendApplication {
+public class YsyAgendaBackendApplication extends SpringBootServletInitializer { // Extender SpringBootServletInitializer
     public static void main(String[] args) {
-        SpringApplication.run(YsyAgendaBackendApplication.class);
+        SpringApplication.run(YsyAgendaBackendApplication.class, args);
     }
 
     @Bean
